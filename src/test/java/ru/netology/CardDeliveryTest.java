eaa214e3d24cc87ed9ee4c6a422ff24ca9052766
Tatiana Rudikova <tatjana_tv@list.ru>
2023-01-31 23:54:30 +0300

    added new tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -32,6 +32,116 @@ public class CardDeliveryTest {
         $("[class='button__text']").click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofMillis(15000))
                 .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + meetingDate));
+    }
 
+    @Test
+    void shouldCityNotInList() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Ковров");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldEmptyInCityField() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldDateLessThreeDays() {
+        String meetingDate = LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='date'] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldHyphenInNameField() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов-Петров Иван");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofMillis(15000))
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + meetingDate));
+    }
+
+    @Test
+    void shouldLatinLettersInNameField() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Ivanov Ivan");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldPhoneWithoutPlusSymbol() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("81111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldMoreElevenCharactersInPhone() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+711111111118");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void shouldLessElevenCharactersInPhone() {
+        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+7111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
